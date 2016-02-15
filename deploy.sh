0b800e33754314d8a29508b5e8c475e39794b823
John Jones <johnjones4@gmail.com>
2016-02-15 13:27:12 -0500

    deploy agent

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git stash
+git pull
+git submodule foreach git pull origin master
+git submodule update
+sudo service cis-scanner stop
+sudo service cis-web stop
+npm install
+grunt dist
+sudo service cis-scanner start
+sudo service cis-web start
